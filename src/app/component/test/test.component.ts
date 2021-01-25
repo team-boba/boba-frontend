6b4f051d0c371e442db69c488ebd56bb7f7f6b8f
impressiveHen <b02505002@gmail.com>
2021-01-25 13:12:52 -0800

    bug: reactive form validation
    Signed-off-by: impressiveHen <b02505002@gmail.com>

---
+++ src/app/component/test/test.component.ts
@@ -0,0 +1,62 @@
+import { FormValidationService } from './../../shared/form-validation/form-validation.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, ValidatorFn } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+  visaTypes = ['h1b', 'opt', 'greencard'];
+  personNames = ['henry', 'angelina', 'boba'];
+
+  constructor(
+    private fb: FormBuilder,
+    private formValidationService: FormValidationService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  employeeForm = this.fb.group({
+    title: ['', [Validators.required]],
+    manager: [''],
+    startDate: ['', [Validators.required]],
+    endDate: ['', ],
+    avatar: [''],
+    car: [''],
+    visaStatus: this.fb.group({
+      visaType: [''],
+      visaStartDate: [''],
+      visaEndDate: ['']
+    }),
+    driverLicense: [''],
+    driverLicenseExpirationDate: ['', [this.formValidationService.dateAfterValidator(null)]]
+  });
+
+  get employeeFormControls() {
+    return this.employeeForm.controls;
+  }
+
+  get visaStatusControls() {
+    return (this.employeeForm.get('visaStatus') as FormGroup).controls;
+  }
+
+  onSubmit() {
+    console.log(this.employeeForm.value);
+  }
+
+  handleFileInput(files: FileList) {
+    this.employeeForm.patchValue({
+      avatar: files.item(0)
+    })
+  }
+
+  validateEndDate(): ValidatorFn {
+    if (!this.employeeForm.value['startDate']) return null;
+    return this.formValidationService.dateAfterValidator(this.employeeForm.value['dateAfter']);
+  }
+
+}
