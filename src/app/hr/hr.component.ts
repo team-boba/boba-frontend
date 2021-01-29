e1ef7f3a02ca8f43dd076c794b14e935425720e8
impressiveHen <b02505002@gmail.com>
2021-01-28 17:26:42 -0800

    nav bar person model
    Signed-off-by: impressiveHen <b02505002@gmail.com>

--- src/app/hr/hr.component.ts
+++ src/app/hr/hr.component.ts
@@ -10,6 +10,11 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 export class HrComponent implements OnInit {
   faCoffee = faCoffee
 
+  testTitle: string = "I983";
+  testDescription: string = "I983 form for OPT";
+  testS3FileName: string = "1611711498205-i983.pdf";
+  testS3FileUrl: string = "https://beaconfire-team-boba.s3-us-west-1.amazonaws.com/1611711498205-i983.pdf";
+
   constructor(private confirmDialogService: ConfirmDialogService) { }
 
   ngOnInit(): void {
